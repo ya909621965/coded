11b7e693cecb5cf2aafed9515f6f7ceb7209f627
ya909621965 <ya9096219657@gmail.com>
2017-05-22 14:21:08 +0400

    Update comparers.js

--- comparers.js
+++ comparers.js
@@ -1,24 +1,21 @@
 import * as strings from './strings';
 
 const bigLettersCount = (str) => {
-  // BEGIN (write your solution here)
+  
     let result= 0;
     for(let i = 0; i<strings.length(str); i++){
  if (strings.toUpperCase(str[i]) === str[i])
  result = result+1;
  }
     return result;
-
-  // END
 };
-
 
 const compare = (first, second) => {
   const firstCount = bigLettersCount(first);
   const secondCount = bigLettersCount(second);
 
-  // BEGIN (write your solution here)
-{if (firstCount>secondCount)
+}
+if (firstCount>secondCount)
 {return 1;}
 if (firstCount<secondCount)
 {return -1;}
@@ -27,7 +24,6 @@ if (firstCount===secondCount)
 
 }
 
-  // END
 };
 
 export const greaterThan = (first, second) =>
